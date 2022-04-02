a4d9d2c00eede3262fe839c36253fd3e1297ca52
FeatheredSnek <dorian.maczka@gmail.com>
2022-04-02 19:24:29 +0200

    refactored generator for unique batches

--- src/data/generator.ts
+++ src/data/generator.ts
@@ -1,45 +1,76 @@
-import premadeLists from "@/data/lists";
-import { Card } from "@/types/index";
+import premadeLists from "@/data/lists"
+import { Card } from "@/types/index"
 
 export default {
-
   lists: premadeLists,
 
-  generateCard (
-      names: string[], 
-      types: string[], 
-      words: string[], 
-      images: string[],
-      statMax: number,
-      statMin: number) : Card {
-    const name = names[Math.floor(Math.random() * names.length)]
-    const type = types[Math.floor(Math.random() * types.length)]
-    const image = images[Math.floor(Math.random() * images.length)]
-    let text = ''
-    const textLength = 8 + Math.floor(Math.random() * 5)
-    for (let i = 0; i < textLength; i++) {
-      const id = Math.floor(Math.random() * words.length)
-      text += `${words[id]} `
+  generateSingleCard(): Card {
+    return {
+      id: this.generateId(),
+      name: this.generateName(this.lists.names),
+      type: this.generateType(this.lists.types),
+      text: this.generateText(this.lists.words),
+      statA: this.generateStat(),
+      statB: this.generateStat(),
+      image: this.generateImage(this.lists.images),
     }
-    text = text.trim()
-    text = text[0].toUpperCase() + text.substring(1,text.length)
-    const id = Math.random().toString(36).slice(2)
-    const statA = Math.floor(Math.random() * (statMax + 1)) + statMin
-    const statB = Math.floor(Math.random() * (statMax + 1)) + statMin
-    return {id, name, type, text, statA, statB, image}
   },
 
-  generateBatch (batchSize: number) : Card[] {
+  generateBatch(batchSize: number): Card[] {
     const batchOutput = []
+    const batchNames = this.lists.names
+      .sort(() => 0.5 - Math.random())
+      .slice(0, batchSize)
+    const batchImages = this.lists.images
+      .sort(() => 0.5 - Math.random())
+      .slice(0, batchSize)
     for (let i = 0; i < batchSize; i++) {
-      batchOutput.push(this.generateCard(
-        this.lists.names,
-        this.lists.types,
-        this.lists.words,
-        this.lists.images,
-        9,
-        0))
+      let name = batchNames.length > 0 ? batchNames.pop() : this.lists.names[0]
+      let image =
+        batchImages.length > 0 ? batchImages.pop() : this.lists.images[0]
+      name = name === undefined ? this.lists.names[0] : name
+      image = image === undefined ? this.lists.images[0] : image
+      const type = this.generateType(this.lists.types)
+      const text = this.generateText(this.lists.words)
+      const statA = this.generateStat()
+      const statB = this.generateStat()
+      const id = this.generateId()
+      batchOutput.push({ id, name, type, text, statA, statB, image })
     }
     return batchOutput
-  }
+  },
+
+  generateText(wordlist: string[]) {
+    let text = ""
+    const textLength = 8 + Math.floor(Math.random() * 5)
+    for (let i = 0; i < textLength; i++) {
+      const id = Math.floor(Math.random() * wordlist.length)
+      text += `${wordlist[id]} `
+    }
+    text = text.trim()
+    text = text[0].toUpperCase() + text.substring(1, text.length)
+    return text
+  },
+
+  generateStat(min = 0, max = 9) {
+    return Math.floor(Math.random() * (max + 1)) + min
+  },
+
+  generateId() {
+    return (
+      Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2)
+    )
+  },
+
+  generateType(typeList: string[]) {
+    return typeList[Math.floor(Math.random() * typeList.length)]
+  },
+
+  generateName(nameList: string[]) {
+    return nameList[Math.floor(Math.random() * nameList.length)]
+  },
+
+  generateImage(imageList: string[]) {
+    return imageList[Math.floor(Math.random() * imageList.length)]
+  },
 }

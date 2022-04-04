0c776064535305cbfb845391842f1581b45fe7ca
FeatheredSnek <dorian.maczka@gmail.com>
2022-04-04 21:26:23 +0200

    fixed page title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: '',
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      title: 'vueDraft'
+    }
+  },
   transpileDependencies: true,
   css: {
     loaderOptions: {
